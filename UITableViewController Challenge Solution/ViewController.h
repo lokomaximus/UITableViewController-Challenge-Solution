cd4fdcbc1a69b2e37d3cc62ca81a41cb73265e5b
Javier Frank <mac@MacBook-Pro-de-mac.local>
2014-10-20 00:11:38 -0500

    Initial Commit

---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Javier Frank on 20/10/14.
+//  Copyright (c) 2014 Javier Frank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
